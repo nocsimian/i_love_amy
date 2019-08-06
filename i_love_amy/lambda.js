d96131c6bccfafca0d2ef622507ca9c612828126
nocsimian <reedjzero@gmail.com>
2019-08-07 06:49:26 +1000

    trying to commit again

--- i_love_amy/lambda.js
+++ i_love_amy/lambda.js
@@ -1,4 +1,38 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+    let receiver = event['receiver'];
+    let sender = event['sender'];
+    let message = event['message'];
+    let isPromotional = true;
+
+     console.log("Sending message", message, "to receiver", receiver);
+
+    sns.publish({
+        Message: message,
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                'DataType': 'String',
+                'StringValue': 'Promotional'
+            },
+            'AWS.SNS.SMS.SenderID': {
+                'DataType': 'String',
+                'StringValue': sender
+            }
+        },
+        PhoneNumber: receiver
+    }).promise()
+        .then(data => {
+            // your code goes here
+            console.log("Sent Message to", receiver);
+            callback(null,data);
+        })
+        .catch(err => {
+            // error handling goes here
+            console.log("Sending Failed",err);
+            callback(err);
+        });
+  
 }
